12a245a93dc58ba5dcc116a890ea572a1e7496e4
PS038353 <PS038353@W1561045.northamerica.cerner.net>
2018-08-05 22:29:04 -0400

    initial git commit

--- public_html/src/js/scripts.js
+++ public_html/src/js/scripts.js
@@ -1,1 +1,23 @@
 // Empty JS for your own code to be here
+function navMinIcon() {
+    var x = document.getElementById("nav-min-icon");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+}
