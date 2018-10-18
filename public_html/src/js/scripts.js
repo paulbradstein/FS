f6ae6e0273e75ce91e8e279c48385b51334d1ff1
PS038353 <PS038353@W1561045.northamerica.cerner.net>
2018-10-17 22:11:43 -0400

    initial git commit

--- public_html/src/js/scripts.js
+++ public_html/src/js/scripts.js
@@ -1,17 +1,17 @@
 $(function () {
     new WOW().init();
 });
-/* jquery function to include html file */
+/* jquery function to include html file
 $(function () {
     $("#includedContent").load("fs18_navbar.html");
-});
+}); */
 /* UniteGallery*/
 jQuery(document).ready(function () {
     jQuery("#gallery").unitegallery({
-        gallery_theme: "tilesgrid",
+        gallery_theme: "tiles",
     });
     jQuery("#gallery_tp").unitegallery({
-        gallery_theme: "tilesgrid",
+        gallery_theme: "tiles",
         tile_enable_textpanel: true,
         tile_textpanel_title_text_align: "center"
     });
